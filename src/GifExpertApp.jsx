81b769c70b00eaf88624df132b002a3272306745
Marco Hernandez <marco7245@hotmail.com>
2023-02-12 20:16:23 -0600

    Nuevo Componente AddCategory

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,10 +1,32 @@
 
 import React from 'react'
+import { useState } from 'react'
+import { AddCategory } from './components/addCategory';
 
 const GifExpertApp = () => {
+
+   const [categories,setCategories] =useState(['One Punch','Goku']);
+
+   const onAddCategory = () =>{
+        setCategories([...categories,'Valorant']);
+   }
+
   return (
     <>
+        {/* Titulo */}
         <h1>GifExpertApp</h1>
+
+        {/* Input */}
+        <AddCategory/>
+
+        {/* Listado de Items */}
+        <button onClick={onAddCategory}>Agregar</button>
+        <ol>
+            {categories.map(category=>{
+                    return <li key={category}>{category}</li>
+                })
+            }
+        </ol>
     </>
 
   )
